a50287d32414271be1e85918edd884ba9417feab
hanagemaru <jibunnha@gmail.com>
2026-09-20 00:50:55 +0900

    Mirror Traffic Stabilizer app RC sw.js

---
+++ prototypes/traffic-stabilizer-app-rc/sw.js
@@ -0,0 +1,45 @@
+const CACHE="traffic-stabilizer-app-v1";
+const CORE=["./","./index.html","./style.css","./game.js","./manifest.webmanifest"];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch",event=>{
+  const req=event.request;
+  if(req.method!=="GET")return;
+  const url=new URL(req.url);
+  if(url.origin!==self.location.origin)return;
+
+  if(req.mode==="navigate"){
+    event.respondWith(
+      fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(cache=>cache.put("./index.html",copy));
+        return res;
+      }).catch(()=>caches.match("./index.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then(cached=>{
+      const network=fetch(req).then(res=>{
+        if(res&&res.ok){
+          const copy=res.clone();
+          caches.open(CACHE).then(cache=>cache.put(req,copy));
+        }
+        return res;
+      }).catch(()=>cached);
+      return cached||network;
+    })
+  );
+});
