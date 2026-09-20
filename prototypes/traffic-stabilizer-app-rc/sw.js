ae81ba44912d8d53a42be3dcad4db976fa50882f
hanagemaru <jibunnha@gmail.com>
2026-09-20 14:37:22 +0900

    Finish syncing Flow Journey app RC sw.js

--- prototypes/traffic-stabilizer-app-rc/sw.js
+++ prototypes/traffic-stabilizer-app-rc/sw.js
@@ -1,4 +1,4 @@
-const CACHE="traffic-stabilizer-app-v2";
+const CACHE="traffic-stabilizer-app-v3";
 const CORE=["./","./index.html","./style.css","./game.js","./manifest.webmanifest","./icon.svg"];
 
 self.addEventListener("install",event=>{
